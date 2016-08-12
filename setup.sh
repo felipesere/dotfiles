6f08f4018211a6ec6a5b21f6d9f9b2d6aefdbd0e
Felipe Sere <felipesere@gmail.com>
2016-08-12 14:03:16 +0100

    Adds an add command that creates a new folder based on a template

--- setup.sh
+++ setup.sh
@@ -2,6 +2,20 @@
 
 DIR=$(pwd)
 
+read -r -d '' TEMPLATE <<-EOF
+#! /bin/sh
+BASE=\$1
+EOF
+
+if [[ $1 == "--add" ]]
+then
+  mkdir $2
+  echo "$TEMPLATE" > $DIR/$2/install.sh
+  echo "$TEMPLATE" > $DIR/$2/cleanup.sh
+  chmod +x $DIR/$2/*.sh
+  exit
+fi
+
 for dir in $(ls -d */)
 do
   project=${dir%?}
