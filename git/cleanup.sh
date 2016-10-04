1f0bdbc159d639634f3809cde3220c1b2da018c7
Felipe Sere <felipesere@gmail.com>
2016-10-04 19:45:12 +0100

    Remove githelpers

--- git/cleanup.sh
+++ git/cleanup.sh
@@ -1,6 +1,5 @@
 #! /bin/sh
 
 unlink $HOME/.gitconfig
-unlink $HOME/.githelper
 unlink $HOME/.gitignore
 
--- git/install.sh
+++ git/install.sh
@@ -2,5 +2,4 @@
 BASE=$1
 
 ln -s $BASE/gitconfig  $HOME/.gitconfig
-ln -s $BASE/githelper  $HOME/.githelper
 ln -s $BASE/gitignore  $HOME/.gitignore
