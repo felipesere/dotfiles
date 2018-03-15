0e736bd7c8b9b14e5897ed1b8b7977b71b52cd1f
Felipe Sere <felipesere@gmail.com>
2018-03-15 23:09:11 +0000

    Tweak prompt by adding battery and removing time and user

--- zsh-minimal/theme.zsh
+++ zsh-minimal/theme.zsh
@@ -1,6 +1,4 @@
 SPACESHIP_PROMPT_ORDER=(
-  time          # Time stampts section
-  user          # Username section
   dir           # Current directory section
   git           # Git section (git_branch + git_status)
   line_sep      # Line break
@@ -8,9 +6,15 @@ SPACESHIP_PROMPT_ORDER=(
   exit_code     # Exit code section
   char          # Prompt character
 )
+SPACESHIP_RPROMPT_ORDER=(
+  battery
+)
+
+SPACESHIP_BATTERY_SHOW=always
+
+SPACESHIP_CHAR_SUFFIX=" "
 
 SPACESHIP_DIR_COLOR="blue"
-
 
 SPACESHIP_GIT_SYMBOL=" "
 SPACESHIP_GIT_STATUS_STASHED=""
