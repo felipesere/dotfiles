08bdeef2d570748ca96e7901811eea7560809cb6
Felipe Sere <felipesere@gmail.com>
2018-03-15 09:22:40 +0000

    Use spaceship prompt

--- zsh-minimal/plugins.zsh
+++ zsh-minimal/plugins.zsh
@@ -1,12 +1,8 @@
 source ~/.dotfiles/zsh-minimal/shrink-path.plugin.zsh
 
 source <(antibody init)
-antibody bundle mafredri/zsh-async
-antibody bundle felipesere/pure
+antibody bundle denysdovhan/spaceship-prompt
 
 antibody bundle willghatch/zsh-cdr
 antibody bundle zsh-users/zsh-syntax-highlighting
 antibody bundle b4b4r07/enhancd
-
-
-export ENHANCD_FILTER="/usr/local/bin/fzf --reverse --height 50%"
---
+++ zsh-minimal/theme.zsh
@@ -0,0 +1,18 @@
+SPACESHIP_PROMPT_ORDER=(
+  time          # Time stampts section
+  user          # Username section
+  dir           # Current directory section
+  git           # Git section (git_branch + git_status)
+  line_sep      # Line break
+  jobs          # Background jobs indicator
+  exit_code     # Exit code section
+  char          # Prompt character
+)
+
+SPACESHIP_GIT_SYMBOL="X" # ???
+SPACESHIP_GIT_STATUS_STASHED=""
+SPACESHIP_GIT_STATUS_PREFIX=" "
+SPACESHIP_GIT_STATUS_SUFFIX=""
+
+SPACESHIP_JOBS_SYMBOL="‼️"
+SPACESHIP_JOBS_SUFFIX="  "
