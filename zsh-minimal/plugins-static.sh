3128b5e91b55592173dda4c04ca5cd2994868dc5
Felipe Sere <felipesere@gmail.com>
2018-05-14 12:01:35 +0100

    Update plugins and replace nvm with just n

---
+++ zsh-minimal/plugins-static.sh
@@ -0,0 +1,4 @@
+source /Users/felipesere/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-denysdovhan-SLASH-spaceship-prompt/spaceship.zsh
+source /Users/felipesere/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-willghatch-SLASH-zsh-cdr/cdr.plugin.zsh
+source /Users/felipesere/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
+source /Users/felipesere/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-b4b4r07-SLASH-enhancd/init.sh
--- zsh-minimal/plugins.zsh
+++
@@ -1,6 +0,0 @@
-source <(antibody init)
-antibody bundle denysdovhan/spaceship-prompt
-
-antibody bundle willghatch/zsh-cdr
-antibody bundle zsh-users/zsh-syntax-highlighting
-antibody bundle b4b4r07/enhancd
