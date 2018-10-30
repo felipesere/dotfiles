45325f44988d50e76991cb8f2bfd68c584ed8a40
Felipe Sere <felipesere@gmail.com>
2018-10-30 09:02:12 +0000

    Update static plugins

--- zsh-minimal/plugins-static.sh
+++ zsh-minimal/plugins-static.sh
@@ -1,4 +1,8 @@
 source /Users/felipesere/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-denysdovhan-SLASH-spaceship-prompt/spaceship.zsh
+fpath+=( /Users/felipesere/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-denysdovhan-SLASH-spaceship-prompt )
 source /Users/felipesere/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-willghatch-SLASH-zsh-cdr/cdr.plugin.zsh
+fpath+=( /Users/felipesere/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-willghatch-SLASH-zsh-cdr )
 source /Users/felipesere/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
+fpath+=( /Users/felipesere/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting )
 source /Users/felipesere/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-b4b4r07-SLASH-enhancd/init.sh
+fpath+=( /Users/felipesere/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-b4b4r07-SLASH-enhancd )
