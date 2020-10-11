52b9da3b8525f5afddcef0bcd58e474608871e89
Felipe Sere <felipesere@gmail.com>
2020-10-11 14:24:10 +0200

    Replace spaceship with starship for prompt

--- zsh-minimal/plugins-static.sh
+++ zsh-minimal/plugins-static.sh
@@ -1,8 +1,6 @@
-source /Users/felipesere/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-denysdovhan-SLASH-spaceship-prompt/spaceship.zsh
-fpath+=( /Users/felipesere/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-denysdovhan-SLASH-spaceship-prompt )
 source /Users/felipesere/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-willghatch-SLASH-zsh-cdr/cdr.plugin.zsh
 fpath+=( /Users/felipesere/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-willghatch-SLASH-zsh-cdr )
 source /Users/felipesere/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
 fpath+=( /Users/felipesere/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting )
-source /Users/felipesere/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-b4b4r07-SLASH-enhancd/init.sh
+source /Users/felipesere/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-b4b4r07-SLASH-enhancd/enhancd.plugin.zsh
 fpath+=( /Users/felipesere/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-b4b4r07-SLASH-enhancd )
