b1e849a599c5c6e8abec77b0abcd34e31d48c1e8
Felipe Sere <felipesere@gmail.com>
2020-11-17 08:36:05 +0100

    Add zsh abbr to have expandable abbreviations

--- zsh-minimal/plugins-static.sh
+++ zsh-minimal/plugins-static.sh
@@ -4,3 +4,5 @@ source /Users/felipesere/Library/Caches/antibody/https-COLON--SLASH--SLASH-githu
 fpath+=( /Users/felipesere/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting )
 source /Users/felipesere/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-b4b4r07-SLASH-enhancd/enhancd.plugin.zsh
 fpath+=( /Users/felipesere/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-b4b4r07-SLASH-enhancd )
+source /Users/felipesere/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-olets-SLASH-zsh-abbr/zsh-abbr.plugin.zsh
+fpath+=( /Users/felipesere/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-olets-SLASH-zsh-abbr )
