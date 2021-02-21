e6ce11f9e12384afd7ee2e36ee2bfb5ecf6a20a6
Felipe Sere <felipesere@gmail.com>
2021-02-21 22:24:50 +0000

    Swap out antigen for zplug

--- zsh-minimal/plugins-static.sh
+++
@@ -1,8 +0,0 @@
-source /Users/felipesere/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-willghatch-SLASH-zsh-cdr/cdr.plugin.zsh
-fpath+=( /Users/felipesere/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-willghatch-SLASH-zsh-cdr )
-source /Users/felipesere/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
-fpath+=( /Users/felipesere/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting )
-source /Users/felipesere/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-b4b4r07-SLASH-enhancd/enhancd.plugin.zsh
-fpath+=( /Users/felipesere/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-b4b4r07-SLASH-enhancd )
-source /Users/felipesere/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-olets-SLASH-zsh-abbr/zsh-abbr.plugin.zsh
-fpath+=( /Users/felipesere/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-olets-SLASH-zsh-abbr )
