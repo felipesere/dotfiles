2f2d1c5927309c674db4ea5be7ab814775987ea8
Felipe Sere <felipesere@gmail.com>
2021-10-03 08:42:41 +0200

    Extract zplug into its own configuration file

---
+++ zsh-minimal/zplug.zsh
@@ -0,0 +1,29 @@
+export ZDOTDIR=$HOME
+
+# Experimental
+if [[ `uname` == "Linux" ]]; then
+  export ZPLUG_HOME=/usr/share/zplug
+elif [[ `uname` == "Darwin" ]]; then
+  export ZPLUG_HOME=/opt/homebrew/opt/zplug
+else
+  echo "Unknown OS!"
+fi
+
+if [[ -v ZPLUG_HOME ]]; then
+  source $ZPLUG_HOME/init.zsh
+  
+  zplug "willghatch/zsh-cdr"
+  zplug "zsh-users/zsh-syntax-highlighting"
+  zplug "b4b4r07/enhancd"
+  zplug "olets/zsh-abbr"
+  
+  # Install plugins if there are plugins that have not been installed
+  if ! zplug check --verbose; then
+      printf "Install? [y/N]: "
+      if read -q; then
+          echo; zplug install
+      fi
+  fi
+  
+  zplug load
+fi
