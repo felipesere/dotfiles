e23ab378a45fcddb670dc18483f874e64ddc464f
Felipe Sere <github@felipesere.com>
2022-12-20 12:01:10 +0100

    Better symlinks

--- bin/symlink-all.sh
+++ bin/symlink-all.sh
@@ -7,9 +7,7 @@ ln -sf $HOME/.dotfiles/bin/theme.sh               $HOME/bin/theme
 ln -sf $HOME/.dotfiles/bat/bat                    $HOME/.bat
 ln -sf $HOME/.dotfiles/git/gitconfig              $HOME/.gitconfig
 ln -sf $HOME/.dotfiles/git/gitignore              $HOME/.gitignore
-ln -sf $HOME/.dotfiles/nvim/init.lua              $HOME/.config/nvim/init.lua
-ln -sf $HOME/.dotfiles/nvim/autoload              $HOME/.config/nvim/autoload
-ln -sf $HOME/.dotfiles/nvim/lua                   $HOME/.config/nvim/lua
+ln -sf $HOME/.dotfiles/nvim                       $HOME/.config/nvim
 ln -sf $HOME/.dotfiles/starship/starship.toml     $HOME/.config/starship.toml
 ln -sf $HOME/.dotfiles/tmux/tmux.conf             $HOME/.tmux.conf
 ln -sf $HOME/.dotfiles/zsh-minimal/zshrc          $HOME/.zshrc
