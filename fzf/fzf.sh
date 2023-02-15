4cb10931a8e6608e1d128a30f5409d4d82f5bb07
Felipe Sere <github@felipesere.com>
2023-02-15 18:26:29 +0000

    Further shrink away unused elements

--- fzf/fzf.sh
+++ fzf/fzf.sh
@@ -8,10 +8,6 @@ fzf="$(command -v fzf 2> /dev/null)" || fzf="$(dirname "$0")/fzf"
 [[ -x "$fzf" ]] || fail 'fzf executable not found'
 
 args=()
-opt=""
-skip=""
-close=""
-term=""
 
 # --height option is not allowed. CTRL-Z is also disabled.
 args=("${args[@]}" "--no-height" "--bind=ctrl-z:ignore")
@@ -45,30 +41,13 @@ opts=$(printf "%q " "${args[@]}")
 
 pppid=$$
 echo -n "trap 'kill -SIGUSR1 -$pppid' EXIT SIGINT SIGTERM;" >> $argsf
-close="; trap - EXIT SIGINT SIGTERM $close"
+close="; trap - EXIT SIGINT SIGTERM"
 
 mkfifo -m o+w $fifo2
-if [[ "$opt" =~ "-E" ]]; then
-  cat $fifo2 &
-  if [[ -n "$term" ]] || [[ -t 0 ]]; then
-    cat <<< "\"$fzf\" $opts > $fifo2; out=\$? $close; exit \$out" >> $argsf
-  else
-    mkfifo $fifo1
-    cat <<< "\"$fzf\" $opts < $fifo1 > $fifo2; out=\$? $close; exit \$out" >> $argsf
-    cat <&0 > $fifo1 &
-  fi
-
-  exit $?
-fi
-
 mkfifo -m o+w $fifo3
-if [[ -n "$term" ]] || [[ -t 0 ]]; then
-  cat <<< "\"$fzf\" $opts > $fifo2; echo \$? > $fifo3 $close" >> $argsf
-else
-  mkfifo $fifo1
-  cat <<< "\"$fzf\" $opts < $fifo1 > $fifo2; echo \$? > $fifo3 $close" >> $argsf
-  cat <&0 > $fifo1 &
-fi
+mkfifo $fifo1
+cat <<< "\"$fzf\" $opts < $fifo1 > $fifo2; echo \$? > $fifo3 $close" >> $argsf
+cat <&0 > $fifo1 &
 "bash -c 'exec -a fzf bash $argsf'" > /dev/null 2>&1 || { "$fzf" "${args[@]}"; exit $?; }
 cat $fifo2
 exit "$(cat $fifo3)"
