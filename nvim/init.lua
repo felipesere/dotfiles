1e0474b4f6b64fc9febd86d90f1310e44fc95c83
Felipe Sere <github@felipesere.com>
2023-05-09 09:59:19 +0100

    Add incremental selection with ALT

--- nvim/init.lua
+++ nvim/init.lua
@@ -241,6 +241,15 @@ require("nvim-treesitter.configs").setup({
   highlight = {
     enable = true
   },
+  incremental_selection = {
+     enable = true,
+     keymaps = {
+        init_selection = "gnn", -- set to `false` to disable one of the mappings
+        node_decremental = "<A-j>",
+        node_incremental = "<A-k>",
+        scope_incremental = "<A-l>",
+     },
+  },
 })
 
 -- requires treesittter, so needs to come after?
