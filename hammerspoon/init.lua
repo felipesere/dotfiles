f06e4879d8ab04fc0fc922802eb6d3b0ed8f902f
Felipe Sere <github@felipesere.com>
2024-03-12 11:40:38 +0000

    Arc has replaced Chrome at work

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -14,7 +14,7 @@ end
 -- Global terminal toggle
 hs.hotkey.bind(mash, "a", function() toggleApp("alacritty") end)
 hs.hotkey.bind(mash, "s", function() toggleApp("Slack") end)
-hs.hotkey.bind(mash, "c", function() toggleApp("Chrome") end)
+hs.hotkey.bind(mash, "c", function() toggleApp("Arc") end)
 hs.hotkey.bind(mash, "n", function() toggleApp("Notion") end)
 
 hs.hotkey.bind(mash, "r", function() hs.reload(); end)
