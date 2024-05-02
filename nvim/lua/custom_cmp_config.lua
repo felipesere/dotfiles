951547cc02a65b4b829b8182d7dcd9fdf6a4bc19
Felipe Sere <github@felipesere.com>
2024-05-02 12:51:50 +0100

    Allow docs for completions to be toggled

--- nvim/lua/custom_cmp_config.lua
+++ nvim/lua/custom_cmp_config.lua
@@ -1,10 +1,16 @@
 local cmp = require("cmp")
 require("luasnip.loaders.from_vscode").lazy_load()
+
 cmp.setup({
   snippet = {
       expand = function(args)
          require("luasnip").lsp_expand(args.body)
       end,
+  },
+  view = {
+    docs = {
+      auto_open = false
+    }
   },
   window = {
        completion = cmp.config.window.bordered(),
@@ -15,11 +21,16 @@ cmp.setup({
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
         ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.abort(),
         ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        -- ["<Tab>"] = cmp.mapping.confirm({ select = true, }),
+        ['<C-g>'] = function()
+        if cmp.visible_docs() then
+          cmp.close_docs()
+        else
+          cmp.open_docs()
+        end
+      end
   }),
 
   sources = cmp.config.sources({
