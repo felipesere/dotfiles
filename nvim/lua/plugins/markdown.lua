9d15403c4ca157f2dc7a24f389923c5de3143115
Felipe Sere <github@felipesere.com>
2024-09-17 11:20:58 +0100

    Add a lovely UI for markdown and cycle through checkboxes

---
+++ nvim/lua/plugins/markdown.lua
@@ -0,0 +1,48 @@
+return {
+  {
+    "epilande/checkbox-cycle.nvim",
+    ft = "markdown",
+    opts = {
+      states = { "[ ]", "[/]", "[x]", "[~]" },
+    },
+    keys = {
+      {
+        "<CR>",
+        "<Cmd>CheckboxCycleNext<CR>",
+        desc = "Checkbox Next",
+        ft = { "markdown" },
+        mode = { "n", "v" },
+      },
+      {
+        "<S-CR>",
+        "<Cmd>CheckboxCyclePrev<CR>",
+        desc = "Checkbox Previous",
+        ft = { "markdown" },
+        mode = { "n", "v" },
+      },
+    },
+  },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" },
+    opts = {
+      checkbox = {
+        custom = {
+          ["in-progress"] = { raw = "[/]", rendered = "󰿦", highlight = "RenderMarkdownWarn" },
+          urgent = { raw = "[!]", rendered = "󰄱", highlight = "RenderMarkdownError" },
+          canceled = { raw = "[~]", rendered = "󰂭", highlight = "RenderMarkdownError" },
+          todo = { raw = "[-]", rendered = "", highlight = "Comment" },
+          forwarded = { raw = "[>]", rendered = "󰒊", highlight = "RenderMarkdownHint" },
+          scheduled = { raw = "[<]", rendered = "󰃰", highlight = "RenderMarkdownHint" },
+          info = { raw = "[i]", rendered = "󰋼", highlight = "RenderMarkdownInfo" },
+          question = { raw = "[?]", rendered = "", highlight = "RenderMarkdownWarn" },
+          idea = { raw = "[I]", rendered = "󰛨", highlight = "RenderMarkdownWarn" },
+          pros = { raw = "[p]", rendered = "󰔓", highlight = "RenderMarkdownSuccess" },
+          cons = { raw = "[c]", rendered = "󰔑", highlight = "RenderMarkdownError" },
+          star = { raw = "[s]", rendered = "󰓎", highlight = "RenderMarkdownWarn" },
+          f = { raw = "[f]", rendered = "󰈸", highlight = "RenderMarkdownH2" },
+        },
+      },
+    },
+  },
+}
