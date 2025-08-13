a0c77ca361ae24b42ea67942fa6dffbe8b374382
Felipe Sere <github@felipesere.com>
2025-08-13 13:47:18 +0100

    nicer window separator

--- nvim/lua/plugins/winsep.lua
+++ nvim/lua/plugins/winsep.lua
@@ -2,6 +2,11 @@ return {
   {
     "nvim-zh/colorful-winsep.nvim",
     config = true,
+    opts = {
+      indicator_for_2wins = {
+        position = "center",
+      },
+    },
     event = { "WinLeave" },
   },
 }
