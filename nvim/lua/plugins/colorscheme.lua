006a25389ef2853043e79fcc08f9b55f27e7b9df
Felipe Sere <github@felipesere.com>
2025-10-16 23:18:36 +0100

    Update plugins

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -16,10 +16,6 @@ return {
     "catppuccin",
     priority = 1000,
     optional = true,
-    opts = function()
-      local bufferline = require("catppuccin.groups.integrations.bufferline")
-      bufferline.get = bufferline.get or bufferline.get_theme
-    end,
   },
   -- { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 }
