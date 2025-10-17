c835af10ad133490c67eb6751c63b6756a31633e
Felipe Sere <github@felipesere.com>
2025-10-17 22:02:06 +0100

    Remove temporary textDocument/diagnostic

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -56,14 +56,3 @@ require("lazy").setup({
     },
   },
 })
-
--- temporary?
-for _, method in ipairs({ "textDocument/diagnostic", "workspace/diagnostic" }) do
-  local default_diagnostic_handler = vim.lsp.handlers[method]
-  vim.lsp.handlers[method] = function(err, result, context, config)
-    if err ~= nil and err.code == -32802 then
-      return
-    end
-    return default_diagnostic_handler(err, result, context, config)
-  end
-end
