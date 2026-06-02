ff81c706b20ee0f491352b59151d079ac5696202
Felipe Sere <github@felipesere.com>
2026-06-02 08:51:43 +0100

    Add tool for PR review

--- nvim/lua/plugins/vcs.lua
+++ nvim/lua/plugins/vcs.lua
@@ -24,4 +24,15 @@ return {
   {
     "rafikdraoui/jj-diffconflicts",
   },
+  {
+    "0xKitsune/pr.nvim",
+    -- or use a local path:
+    -- dir = "~/path/to/pr.nvim",
+    dependencies = {
+      "nvim-telescope/telescope.nvim", -- optional
+    },
+    config = function()
+      require("pr").setup()
+    end,
+  },
 }
