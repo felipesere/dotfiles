174529cbebb47a35342fd3e63aaf293373977623
Felipe Sere <github@felipesere.com>
2026-07-23 16:39:34 +0100

    Remove customisation from theme switcher

--- bin/theme.sh
+++ bin/theme.sh
@@ -8,32 +8,14 @@ light | dark) ;;
   ;;
 esac
 
-mode=$1
-case $mode in
+case $1 in
 dark)
   is_dark="true"
-  is_light="false"
   ;;
 light)
   is_dark="false"
-  is_light="true"
   ;;
 esac
-
-## Alacritty
-ln -sf ~/.dotfiles/alacritty/alacritty.$mode.toml ~/.alacritty.theme.toml
-touch ~/.config/alacritty/alacritty.toml
-
-## Tmux
-if [[ -v TMUX ]]; then
-  tmux source-file ~/.dotfiles/tmux/${mode}.tmux
-fi
-
-## Delta
-sd '^features = .*' "features = ${mode}" ~/.dotfiles/git/gitconfig
-
-## Vim
-echo $mode >~/.theme
 
 ## OSX
 osascript -e "tell app \"System Events\" to tell appearance preferences to set dark mode to ${is_dark}"
